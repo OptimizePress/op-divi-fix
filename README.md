4ca4b39cd853c053bc7b6c52f8b73f00337d250a
Zvonko Biskup <coder@codeforest.net>
2016-04-07 17:17:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# OptimizePress Divi Theme Fix
+
+##Description
+
+Description: Fixes Divi Theme by removing some of it's action while using LiveEditor
+
+##Installation
+
+1. Click Download ZIP button on the right
+2. Install plugin archive through WordPress' Add plugin screen
+3. Activate the plugin
+
+##Requirements
+* Requires at least: 3.5
+* Stable tag: 1.0
+* License: GPLv2 or later
+* License URI: [http://www.gnu.org/licenses/gpl-2.0.html](http://www.gnu.org/licenses/gpl-2.0.html)
+
+
+##Changelog
+
+####1.0
+* Initial release
---
+++ optimizepress-divi-fix.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ *
+ * @link              http://www.optimizepress.com/
+ * @since             1.0.0
+ * @package           Op_Divi_Fix
+ *
+ * @wordpress-plugin
+ * Plugin Name:       OptimizePress Divi theme fix
+ * Plugin URI:        http://www.optimizepress.com/
+ * Description:       Fix for Live Editor if Divi theme or plugins are used
+ * Version:           1.0.0
+ * Author:            OptimizePress
+ * Author URI:        http://www.optimizepress.com/
+ * Text Domain:       op-divi-fix
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+add_action('init' , 'opRemoveDiviCSSStyles', 99);
+
+/**
+ * Remove some Divi theme actions to preserve compatibility of Live Editor.
+ */
+function opRemoveDiviCSSStyles()
+{
+	$pageBuilder = false;
+	if (isset($_GET)) {
+		if (array_key_exists('page', $_GET)) {
+			$pageBuilder = ($_GET['page'] == 'optimizepress-page-builder' ) ? true : false;
+		}
+	}
+
+	if ($pageBuilder) {
+		remove_action('wp_head', 'et_divi_add_customizer_css');
+		remove_action('wp_enqueue_scripts', 'et_builder_load_modules_styles', 11);
+		remove_action('wp_footer', 'et_pb_maybe_add_advanced_styles');
+	}
+}
